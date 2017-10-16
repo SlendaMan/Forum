b31735fd6d254f702e3159f90eb6964c9bf71d1d
SlendaMan <32059783+SlendaMan@users.noreply.github.com>
2017-10-16 12:34:54 +0200

    Add files via upload

--- comment.php
+++ comment.php
@@ -1,19 +1,51 @@
 <?php
-	include("config.php");
-	echo "hej";
-	if($con = connect_db()) {
-		$sql ="select tid, heading, content, name from thread, user where thread.uid = user.id";
+if (!(session_status() == PHP_SESSION_ACTIVE)) {
+	session_start();
+}
+	include_once("config.php");
+	
+if($con = connect_db()) {
+
+	$tid = htmlentities($_GET['tid']);
+		$sql ="select tid, heading, content, name from thread, user where thread.tid = $tid";
 		$result = $con->query($sql);
+		
+		if($result->num_rows > 0) {
+			echo "<ul>";
+			while($row = $result->fetch_assoc()) {
+				$tid = $row['tid'];
+				$heading = $row['heading'];
+				$name=$row['name'];
 
-	$tid = $row['tid'];
-	$heading = $row['heading'];
-	$threadtext=$row['name'];
 
-	echo  "<p class ='heading'> $heading </p>". " by $threadtext <br>".$row['content']; 
+			echo  "<p class ='heading'> $heading </p>". " by $name <br>".$row['content']; 
+	
 				
-			
+		}	
+		}
+
+		if(isset($_POST['comment'])){
+			include_once("config.php");
+					$com=$_POST['comment'];
+					$sql = "insert into answer values(0,?)";
+				
+					if($s = $con->prepare($sql)) {
+						$s->bind_param("s",$com);
+						$s->execute();
+						$s->close();
+						
+					}
+				
+			}
+			else{
+				header("location:commentforum.php");
+			}
+				
+		
 
 		
+		
 	}
+		
 
 ?>
---
+++ commentforum.php
@@ -0,0 +1,9 @@
+<form method="POST">
+	<table>
+		<tr><td>Comment:</td><td><textarea name="comment"></textarea></td></tr>
+			
+		</td></tr>
+		
+	<tr><td colspan="2"><input type="submit" value="Comment"></td></tr>
+	</table>
+</form>
